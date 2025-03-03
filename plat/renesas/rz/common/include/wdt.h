5e73ea26d4ab8b927a2b8bf1cc38ef3a83ae6e63
Hugo Villeneuve <hvilleneuve@dimonoff.com>
2025-03-03 15:40:46 -0500

    Add watchdog timer driver include file
    Add missing include file.
    
    Signed-off-by: Hugo Villeneuve <hvilleneuve@dimonoff.com>

---
+++ plat/renesas/rz/common/include/wdt.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020, Renesas Electronics Corporation. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __WDT_H__
+#define __WDT_H__
+
+void rzg_wdt_start(uint32_t timeout_sec);
+
+#endif /* __WDT_H__ */
