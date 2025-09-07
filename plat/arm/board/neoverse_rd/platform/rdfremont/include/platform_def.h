jolivain/trusted-firmware-a

feat(rdfremont): initialize the rse comms driver
Define platform specific API to fetch base address for secure or root
MHUv3 between AP-RSE invoke rse-comms driver initialization bl31
platform setup stage.

Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
Change-Id: Id79bcdb2fda6cdf394f4e02f67d1c1a44d5ddf23

plat/arm/board/neoverse_rd/platform/rdfremont/include/platform_def.h
@@ -40,4 +40,12 @@
 /* Address bits */
 #define NRD_ADDR_BITS_PER_CHIP		U(36)  /* 64GB */
 
+/*
+ * In the current implementation, the RoT Service request that requires the
+ * biggest message buffer is the RSE_DELEGATED_ATTEST_GET_PLATFORM_TOKEN. The
+ * maximum required buffer size is calculated based on the platform-specific
+ * needs of this request.
+ */
+#define PLAT_RSE_COMMS_PAYLOAD_MAX_SIZE	UL(0x800)
+
 #endif /* PLATFORM_DEF_H */

SolidRun/arm-trusted-firmware

plat/arm/board/neoverse_rd/platform/rdfremont/include/platform_def.h
@@ -40,4 +40,12 @@
 /* Address bits */
 #define NRD_ADDR_BITS_PER_CHIP		U(36)  /* 64GB */
 
+/*
+ * In the current implementation, the RoT Service request that requires the
+ * biggest message buffer is the RSE_DELEGATED_ATTEST_GET_PLATFORM_TOKEN. The
+ * maximum required buffer size is calculated based on the platform-specific
+ * needs of this request.
+ */
+#define PLAT_RSE_COMMS_PAYLOAD_MAX_SIZE	UL(0x1000)
+
 #endif /* PLATFORM_DEF_H */